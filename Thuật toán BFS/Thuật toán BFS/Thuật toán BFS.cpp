4e84582c1dfa817a596c865254240d24b39a2ce8
Sahb9 <71537975+Sahb9@users.noreply.github.com>
2021-03-21 17:20:55 +0700

    Mô tả về 2 thuật toán
    .

---
+++ Thuật toán BFS/Thuật toán BFS/Thuật toán BFS.cpp
@@ -0,0 +1,57 @@
+﻿// Thuat toan BFS.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include <iostream>
+#include <fstream>
+#include <queue>
+using namespace std;
+int n;
+int matrix[1001][1001];
+bool visited[1001];
+void Init();
+void BFS(int u);
+int main()
+{
+	int u;
+	Init();
+	cout << "Chon dinh bat dau: ";
+	cin >> u;
+	BFS(u);
+	return 0;
+}
+//nhập dữ liệu
+void Init()	// gồm 1 mảng 2 chiều , chỉ có số 0 và 1
+{
+
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			cin >> matrix[i][j];
+		}
+		visited[i] = false;
+		//đánh dấu chưa đi qua
+	}
+}
+
+void BFS(int u)
+{
+	cout << "BFS(" << u << "): ";
+	queue<int> Q;
+	Q.push(u);
+	visited[u] = true;
+	while (!Q.empty())
+	{
+		int s = Q.front();
+		cout << s << " ";
+		Q.pop();
+		for (int t = 1; t <= n; t++)
+		{
+			if (visited[t] == false && matrix[s][t] == 1)
+			{
+				Q.push(t);
+				// đánh dấu đường đã đi qua
+ 				visited[t] = true;
+			}
+		}
+	}
+}
---
+++ Thuật toán USC/Thuật toán USC/Thuật toán USC.cpp
@@ -0,0 +1,106 @@
+﻿#include<iostream>
+#include <conio.h>  
+using namespace std;
+#define MAX 100
+
+int edge[MAX][MAX]
+{
+	//0, 1, 2, 3, 4, 5, 6, 7
+	 {0, 0, 0, 0, 0, 0, 0, 0}, //row 0
+	 {0, 0, 5, 0, 3, 0, 0, 0}, //row 1
+	 {0, 0, 0, 1, 0, 0, 0, 0}, //row 2
+	 {0, 0, 0, 0, 0, 6, 0, 8}, //row 3
+	 {0, 0, 0, 0, 0, 2, 2, 0}, //row 4
+	 {0, 0, 4, 0, 0, 0, 0, 0}, //row 5
+	 {0, 0, 0, 0, 0, 0, 0, 3}, //row 6
+	 {0, 0, 0, 0, 0, 4, 0, 0}, //row 7
+};
+//chứa matrix trọng số
+int trace[MAX]; //đường đi
+int numV = 7 /*số điểm*/,
+finish = 7 /*điểm kết thúc*/,
+start = 1 /*bắt đầu*/,
+front = 1, // điểm đầu hàng đợi ưu tiên
+rear = 1,  //điểm cuối hàng đợi ưu tiên
+u, v;
+typedef struct node
+{
+	int vertex/* điểm*/;
+	int cost /* trọng số*/;
+}node;
+node queue[MAX];
+void enqueue(int vertex, int cost);
+node dequeue();
+int isEmty();
+void ucs();
+int main()
+{
+	ucs();
+	if (trace[finish] == 0)
+		cout << "not found";
+	else
+	{
+		while (finish != start)
+		{
+			printf_s("%d <-", finish);
+			finish = trace[finish];
+		}
+		cout << start;
+	}
+	return 0;
+	//getch();
+}
+void enqueue(int vertex, int cost)
+{
+	node v;
+	v.vertex = vertex;
+	v.cost = cost;
+	int index;
+	for (index = front; index <= rear; index++) //tìm vị trí hàng đợi ưu tiên
+		if (v.cost < queue[rear].cost) //điểm hiện tại có chi phí nhỏ hơn chi phí của điểm trong hàng đợi ưu tiên
+			break;
+	if (index == rear + 1) //điểm đang xét có chi phí lớn nhất 
+	{
+		rear++;
+		queue[rear] = v; //được đưa vào cuối hàng đợi
+	}
+	else
+	{
+		for (int k = rear; k >= index; k--) //chèn điểm đang xét vào vị trí nào đó trong hàng đợi
+			queue[k + 1] = queue[k];
+		queue[index] = v;
+		rear++;
+	}
+}
+node dequeue()
+{
+	node u = queue[front];
+	front++;
+	return u;
+}
+int isEmty()
+{
+	if (front > rear) //kiểm tra hàng đợi có bị rỗng không
+		return 1;
+	return 0;
+}
+void ucs()
+{
+	node nodestart;
+	nodestart.vertex = start;
+	nodestart.cost = 0;
+	queue[front] = nodestart;
+	trace[front] = -1;
+	do
+	{
+		u = dequeue().vertex;
+		if (u == finish)
+			break;
+		for (int v = 1; v <= numV; v++)
+			if (edge[u][v] != 0 /*có đường đi từ u tới v?*/ && trace[v] == 0 /*điểm v đã được xét?*/)
+			{
+				enqueue(v, edge[u][v]);
+				trace[v] = u;
+			}
+	} while (!isEmty());
+}
